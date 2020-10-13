2833965317ed140c2fbd730b912db242ae5f6098
Asdrubal Ivan Suarez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2020-10-13 13:54:51 -0300

    Finishing boilerplate

---
+++ app.py
@@ -0,0 +1,53 @@
+from dotenv import load_dotenv
+from flask import Flask, jsonify
+from flask_restful import Api
+from flask_migrate import Migrate, upgrade
+import os
+
+from marshmallow import ValidationError
+
+from ma import ma
+from db import db
+from resources.user import (
+    UserRegister,
+    UserLogin,
+    User,
+    # TokenRefresh,
+    # UserLogout,
+    # SetPassword,
+)
+
+load_dotenv(".env", verbose=True)
+
+app = Flask(__name__)
+app.secret_key = os.environ.get("APP_SECRET_KEY")
+app.config.from_object("default_config")
+app.config.from_envvar("APPLICATION_SETTINGS")
+app.config["MAX_CONTENT_LENGTH"] = 10 * 1024 * 1024
+api = Api(app)
+migrate = Migrate(app, db)
+
+
+# Here we have to run alembic first
+@app.before_first_request
+def create_tables():
+    upgrade()
+
+
+@app.errorhandler(ValidationError)
+def handle_marshmallow_error(err):
+    return jsonify(err.messages), 400
+
+
+api.add_resource(UserRegister, "/register")
+api.add_resource(User, "/user/<int:user_id>")
+api.add_resource(UserLogin, "/login")
+#api.add_resource(TokenRefresh, "/refresh")
+#api.add_resource(UserLogout, "/logout")
+#api.add_resource(SetPassword, "/user/password")
+
+db.init_app(app)
+
+if __name__ == "__main__":
+    ma.init_app(app)
+    app.run(port=5000)
---
+++ decorators.py
@@ -0,0 +1,19 @@
+from functools import wraps
+from flask import request
+from firebase_admin import auth
+
+
+def check_token(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if not request.headers.get("authorization"):
+            return {"message": "No token provided"}, 400
+        try:
+            user = auth.verify_id_token(request.headers["authorization"])
+            request.user = user
+        except:
+            return {"message": "Invalid token provided."}, 400
+        return f(*args, **kwargs)
+
+    wrap.is_checked_by_token = True
+    return wrap
