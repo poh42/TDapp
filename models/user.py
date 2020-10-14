7b394e2278d07215549b160d098bf9af528b5cd2
Asdrubal Ivan Suarez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2020-10-14 14:05:17 -0300

    Created find by email classmethod on models.user

--- models/user.py
+++ models/user.py
@@ -23,6 +23,10 @@ class UserModel(db.Model):
     def find_by_username(cls, username):
         return cls.query.filter_by(username=username).first()
 
+    @classmethod
+    def find_by_email(cls, email):
+        return cls.query.filter_by(email=email).first()
+
     def save(self):
         db.session.add(self)
         db.session.commit()
