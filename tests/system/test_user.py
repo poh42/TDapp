16712613b9c1cf0bc4692b3d75f80121a424f706
Asdrubal Ivan Suarez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2020-10-14 12:04:18 -0300

    Added register tests

---
+++ tests/system/test_user.py
@@ -0,0 +1,37 @@
+from tests.base import BaseAPITestCase
+import unittest
+from unittest.mock import patch
+import json
+from models.user import UserModel
+
+
+class TestUserEndpoints(BaseAPITestCase):
+    def test_something(self):
+        email = "test@topdog.com"
+        password = "Pa55w0rd"
+        username = "asdrubal"
+
+        class TestClass:
+            uid = 1
+
+        with self.test_client() as c:
+            with patch("firebase_admin.auth.create_user") as patched_create_user:
+                patched_create_user.side_effect = (TestClass(),)
+                data = json.dumps(
+                    dict(email=email, password=password, username=username)
+                )
+                rv = c.post("/register", data=data, content_type="application/json")
+                json_data = rv.get_json()
+                self.assertEqual(
+                    "User creation successful",
+                    json_data["message"],
+                    "Message is incorrect",
+                )
+                self.assertIsNotNone(
+                    UserModel.find_by_username(username), "User creation failed"
+                )
+                self.assertEqual(rv.status_code, 201, "Wrong status code")
+
+
+if __name__ == "__main__":
+    unittest.main()
