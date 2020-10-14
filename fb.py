b14e1ccd755de2a9d3ce6b8edee0842a7192e605
Asdrubal Ivan Suarez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2020-10-14 10:20:44 -0300

    Used os.path.dirname for credentials files of firebase

--- fb.py
+++ fb.py
@@ -3,11 +3,11 @@ import pyrebase
 from firebase_admin import credentials
 import firebase_admin
 import json
-
-cred = credentials.Certificate("fbAdminConfig.json")
+import os
+file_dir = os.path.dirname(__file__)
+cred = credentials.Certificate(os.path.join(file_dir, "fbAdminConfig.json"))
 
 # This is needed so tests run correctly
 # TODO check if this can be done without using this comparison
-if len(firebase_admin._apps) == 0:
-    firebase = firebase_admin.initialize_app(cred)
-    pb = pyrebase.initialize_app(json.load(open("fbconfig.json")))  # Data source
+firebase = firebase_admin.initialize_app(cred)
+pb = pyrebase.initialize_app(json.load(open(os.path.join(file_dir,"fbconfig.json"))))  # Data source
