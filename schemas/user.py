afc740fed48017edf6852ed064d535194191973b
Asdrubal Ivan Suarez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2020-10-15 16:48:35 -0300

    Adding validations to user schema

--- schemas/user.py
+++ schemas/user.py
@@ -1,11 +1,31 @@
 from ma import ma
 from models.user import UserModel
-from marshmallow import fields, validate
+from marshmallow import fields, validate, validates_schema
+from utils.validation import validate_lower_upper_fields
 
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
-    email = fields.Email()
+    email = fields.Email(required=True)
     password = fields.String(validate=validate.Length(min=6), required=True)
+    playing_hours_begin = fields.Integer(validate=validate.Range(0, 23))
+    playing_hours_end = fields.Integer(validate=validate.Range(0, 23))
+
+    @validates_schema
+    def validate_related_fields(self, data, **kwargs):
+        validate_lower_upper_fields(
+            data,
+            "playing_hours_begin",
+            "playing_hours_end",
+            "playing_hours_begin and playing_hours_end must be specified together",
+            "playing_hours_begin should be smaller or equal to playing_hours_end",
+        )
+        validate_lower_upper_fields(
+            data,
+            "range_bet_low",
+            "range_bet_high",
+            "range_bet_low and range_bet_high must be specified together",
+            "range_bet_low should be smaller or equal to range_bet_high",
+        )
 
     class Meta:
         model = UserModel
