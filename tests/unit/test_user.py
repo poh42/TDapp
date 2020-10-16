38ac036eb64617bbb22873a8ec249dd236ec0951
Asdrubal Ivan Suarez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2020-10-16 10:58:27 -0300

    Adding unit tests for user resource

--- tests/unit/test_user.py
+++ tests/unit/test_user.py
@@ -1,5 +1,5 @@
 import unittest
-from resources.user import SetAdminStatus
+from resources.user import SetAdminStatus, User
 
 
 class TestUserEndpointDecorators(unittest.TestCase):
@@ -11,4 +11,13 @@ class TestUserEndpointDecorators(unittest.TestCase):
         self.assertTrue(
             SetAdminStatus.post.is_checked_by_admin_claim,
             "Endpoint can be accessed by other users that are not administrators",
+        )
+
+    def test_user(self):
+        self.assertTrue(
+            User.put.is_checked_by_token, "Endpoint is not protected by token"
+        )
+        self.assertTrue(
+            User.put.is_checked_by_admin_or_user,
+            "Endpoint should be accessed by admin or by the current user only",
         )
