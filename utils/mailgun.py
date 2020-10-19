ca71b2a2dcd8c3c4a666a261f2c7ef97414f7059
Asdrubal Ivan Suarez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2020-10-19 15:03:32 -0300

    Adding mailgun utils

--- utils/mailgun.py
+++ utils/mailgun.py
@@ -1,7 +1,6 @@
 from typing import List
 from requests import Response, post
 import os
-from libs.strings import gettext
 
 
 class MailgunException(Exception):
@@ -12,17 +11,19 @@ class MailgunException(Exception):
 class Mailgun:
     MAILGUN_DOMAIN = os.environ.get("MAILGUN_DOMAIN")
     MAILGUN_API_KEY = os.environ.get("MAILGUN_API_KEY")
-    FROM_EMAIL = "mailgun@sandbox0b58acbe9dc14d32858dc81ebca23bf2.mailgun.org"
-    FROM_TITLE = "Stores REST API"
+    FROM_EMAIL = os.environ.get("MAILGUN_FROM_EMAIL")
+    # FROM_EMAIL = "mailgun@sandbox0b58acbe9dc14d32858dc81ebca23bf2.mailgun.org"
+    FROM_TITLE = os.environ.get("MAILGUN_FROM_TITLE")
+    # FROM_TITLE = "Stores REST API"
 
     @classmethod
     def send_email(
         cls, email: List[str], subject: str, text: str, html: str
     ) -> Response:
         if cls.MAILGUN_DOMAIN is None:
-            raise MailgunException(gettext("mailgun_failed_load_domain"))
+            raise MailgunException("No mailgun domain specified")
         if cls.MAILGUN_API_KEY is None:
-            raise MailgunException(gettext("mailgun_failed_load_api_key"))
+            raise MailgunException("Failed to load mailgun API key")
         response = post(
             f"https://api.mailgun.net/v3/{cls.MAILGUN_DOMAIN}/messages",
             auth=("api", cls.MAILGUN_API_KEY),
@@ -36,5 +37,5 @@ class Mailgun:
             timeout=5,
         )
         if response.status_code != 200:
-            raise MailgunException(gettext("mailgun_error_send_email"))
+            raise MailgunException("Error sending email")
         return response
