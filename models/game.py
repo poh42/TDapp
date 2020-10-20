837fe5dd6253088a0e8cc838bf9d1834bab7b972
Asdrubal Ivan Suarez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2020-10-20 17:31:13 -0300

    Adding game model

---
+++ models/game.py
@@ -0,0 +1,15 @@
+from db import db
+from sqlalchemy.sql import func
+
+
+class GameModel(db.Model):
+    __tablename__ = "games"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    image = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(db.DateTime, onupdate=func.now())
+    # challenges = db.relationship(
+    #    "ChallengeModel", lazy="dynamic", cascade="all, delete-orphan"
+    # )
