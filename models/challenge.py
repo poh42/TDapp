449fb56201cd4baa72d6c641f92817c3116470da
Asdrubal Ivan Suarez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2020-10-20 17:29:38 -0300

    Adding challenge model

---
+++ models/challenge.py
@@ -0,0 +1,22 @@
+from db import db
+from sqlalchemy.sql import func
+
+
+class ChallengeModel(db.Model):
+    __tablename__ = "challenges"
+    id = db.Column(db.Integer, primary_key=True)
+    type = db.Column(db.String(45), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+    game_id = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    buy_in = db.Column(db.Numeric(precision=10, scale=2), nullable=False)
+    reward = db.Column(db.Numeric(precision=10, scale=2), nullable=False)
+    status = db.Column(db.String(45), nullable=False)
+    due_date = db.Column(db.DateTime, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(db.DateTime, onupdate=func.now())
+    # game = db.relationship("GameModel")
+
+    @classmethod
+    def find_by_id(cls, _id):
+        cls.query.filter_by(id=_id).first()
