25a06121db410d7a8b90613d2d3b7fda64aa36bd
Asdrubal Ivan Suarez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2020-11-09 13:16:23 -0300

    Fixes comments made by Maureen on #33 regarding Games not found messages

--- app/resources/game.py
+++ app/resources/game.py
@@ -2,6 +2,7 @@ from flask_restful import Resource
 from sqlalchemy.sql import text
 from db import db
 from decorators import optional_check_token
+from models.console import ConsoleModel
 from models.game import GameModel
 from schemas.game import GameSchema
 from utils.claims import get_claims, is_admin
@@ -12,14 +13,20 @@ game_schema = GameSchema()
 class GamesByConsole(Resource):
     @classmethod
     def get(cls, console_id):
+        if not ConsoleModel.console_id_exists(console_id):
+            return {"message": "Console not found"}, 400
         sql = """
-        select g.id, g.name from games g
+        select g.id, g.name, g.image from games g
             inner join games_has_consoles ghc on g.id = ghc.game_id 
             inner join consoles c on c.id = ghc.console_id 
-        where c.id = :id
+        where c.id = :id AND g.is_active = True
         """
         results = db.engine.execute(text(sql), id=console_id).fetchall()
-        return {"message": "Games found", "games": [dict(r) for r in results]}
+        if len(results) > 0:
+            message = "Games found"
+        else:
+            message = "Games not found"
+        return {"message": message, "games": [dict(r) for r in results]}
 
 
 class Games(Resource):
