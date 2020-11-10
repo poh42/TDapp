5d1f44abec5fc9d26884916c8a5ac75fe601f788
Asdrubal Ivan Suarez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2020-11-10 14:11:34 -0300

    Adding is_friend of method

--- app/models/friendship.py
+++ app/models/friendship.py
@@ -1,3 +1,5 @@
+from sqlalchemy import text
+
 from db import db
 
 friendship_table = db.Table(
@@ -5,3 +7,11 @@ friendship_table = db.Table(
     db.Column("follower_id", db.Integer, db.ForeignKey("users.id"), primary_key=True),
     db.Column("followed_id", db.Integer, db.ForeignKey("users.id"), primary_key=True),
 )
+
+
+def is_friend_of_user(user_1_id, user_2_id):
+    sql = "SELECT 1 from friendships f WHERE f.follower_id = :user_1_id AND f.followed_id = :user_2_id LIMIT 1"
+    results = db.engine.execute(
+        text(sql), user_1_id=user_1_id, user_2_id=user_2_id
+    ).fetchall()
+    return len(results) > 0
