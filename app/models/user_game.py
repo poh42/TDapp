c937112d7755bc33d9959a2d1c510210905485e4
Asdrubal Ivan Suarez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2020-11-10 17:52:25 -0300

    Adding constraint to user_game

--- app/models/user_game.py
+++ app/models/user_game.py
@@ -3,6 +3,11 @@ from db import db
 
 class UserGameModel(db.Model):
     __tablename__ = "user_games"
+    __table_args__ = (
+        db.UniqueConstraint(
+            "console_id", "game_id", "user_id", name="unique_user_game_constraint"
+        ),
+    )
     id = db.Column(db.Integer, primary_key=True)
     console_id = db.Column(db.Integer, db.ForeignKey("consoles.id"))
     game_id = db.Column(db.Integer, db.ForeignKey("games.id"))
