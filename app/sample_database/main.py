64de45afe7472827632495eebf67782b08d63ef8
Asdrubal Ivan Suarez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2020-11-19 18:33:59 -0300

    Adding games to consoles

--- app/sample_database/main.py
+++ app/sample_database/main.py
@@ -1,8 +1,14 @@
 from sample_database.console import save as save_consoles
 from sample_database.game import save as save_games
+from sample_database.game_has_console import save as save_game_has_consoles
 
 
 def create_fixtures():
     consoles = save_consoles()
     games = save_games()
-    return {"consoles": consoles, "games": games}
+    game_has_consoles = save_game_has_consoles(games, consoles)
+    return {
+        "consoles": consoles,
+        "games": games,
+        "game_has_consoles": game_has_consoles,
+    }
