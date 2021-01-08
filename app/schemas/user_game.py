aa53f398a9b183537a39cf6137405b844c2c45ef
Asdrubal Suarez <asdrubal.ivan.suarez.rivera@gmail.com>
2021-01-08 13:24:03 -0300

    Adding validations

--- app/schemas/user_game.py
+++ app/schemas/user_game.py
@@ -1,8 +1,31 @@
 from ma import ma
 from models.user_game import UserGameModel
-from marshmallow import fields
+from models.console import ConsoleModel
+from models.game import GameModel
+from marshmallow import fields, ValidationError, Schema
 
 from schemas.console import ConsoleSchema
+
+
+def console_must_exist(_id):
+    if not _id:
+        raise ValidationError("Data not provided")
+    console_exists = ConsoleModel.console_id_exists(_id)
+    if not console_exists:
+        raise ValidationError("Console id should be valid")
+
+
+def game_must_exist(_id):
+    if not _id:
+        raise ValidationError("Data not provided")
+    game = GameModel.find_by_id(_id)
+    if game is None:
+        raise ValidationError("Game id should be valid")
+
+
+class UserGameSchemaWithoutModel(Schema):
+    game_id = fields.Integer(required=True, validate=game_must_exist)
+    console_id = fields.Integer(required=True, validate=console_must_exist)
 
 
 class BaseUserGameSchema(ma.SQLAlchemyAutoSchema):
