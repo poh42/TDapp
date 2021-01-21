fac84a803ed703660d868367994a8817d3b7f75a
Asdrubal Suarez <asdrubal.ivan.suarez.rivera@gmail.com>
2021-01-21 11:51:39 -0300

    Finished schema

--- app/schemas/dispute.py
+++ app/schemas/dispute.py
@@ -1,6 +1,9 @@
 from ma import ma
+from models.challenge_ import ChallengeModel
 from models.dispute import DisputeModel
-from marshmallow import fields
+from marshmallow import fields, Schema, ValidationError, validates_schema
+
+from models.user import UserModel
 
 
 class DisputeSchema(ma.SQLAlchemyAutoSchema):
@@ -20,3 +23,24 @@ class DisputeAdminSchema(ma.SQLAlchemyAutoSchema):
         model = DisputeModel
         dump_only = ("id", "created_at", "updated_at", "user_id")
         load_instance = True
+
+
+def validate_challenge_id(_id):
+    challenge = ChallengeModel.find_by_id(_id)
+    if challenge is None:
+        raise ValidationError("Challenge should be valid")
+
+
+def validate_user_id(_id):
+    user = UserModel.find_by_id(_id)
+    if user is None:
+        raise ValidationError("User id should be valid")
+
+
+class SettleDisputeSchema(Schema):
+    # challenge_id = fields.Integer(required=True, validate=validate_challenge_id)
+    score_player_1 = fields.Integer(required=True)
+    score_player_2 = fields.Integer(required=True)
+    # user_id_1 = fields.Integer(required=True, validate=validate_user_id)
+    # user_id_2 = fields.Integer(required=True, validate=validate_user_id)
+    winner_id = fields.Integer(required=True, validate=validate_user_id)
