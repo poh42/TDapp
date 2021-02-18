536928f0447a150ee1f8b22cc1e46615ffcb5551
Asdrubal Suarez <asdrubal.ivan.suarez.rivera@gmail.com>
2021-02-18 18:41:15 -0300

    Sending messages from twilio

--- app/sms/main.py
+++ app/sms/main.py
@@ -1,10 +1,37 @@
 from models.challenge_user import ChallengeUserModel
+from twilio.rest import Client
+import os
+
+# Your Account Sid and Auth Token from twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
+twilio_from_phone = os.environ.get("TWILIO_FROM_PHONE")
+
+if account_sid is None:
+    print("Warning: Twilio account sid not set")
+
+if auth_token is None:
+    print("Warning: Twilio auth token not set")
+
+if twilio_from_phone is None:
+    print("Warning: Twilio from phone not set")
+
+print(account_sid, auth_token, twilio_from_phone)
+
+client = Client(account_sid, auth_token)
 
 
 def send_sms(number):
     print("sending sms", number)
     if number is not None:
-        print("message sent")
+        message = client.messages.create(
+            body="Hola mundo",
+            from_=twilio_from_phone,
+            to=number,
+        )
+        print("message sent", message.sid)
+        return True
     return False
 
 
@@ -26,7 +53,6 @@ def send_message_challenge_user(challenge_user: ChallengeUserModel):
                 challenge_user.challenger_sms_sent = True
     if modified:
         challenge_user.save_to_db()
-
 
 
 """
