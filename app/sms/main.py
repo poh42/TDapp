117b224dbcb27a5a52f4fe423144487707efd073
Asdrubal Suarez <asdrubal.ivan.suarez.rivera@gmail.com>
2021-02-22 17:11:44 -0300

    Adding message send

--- app/sms/main.py
+++ app/sms/main.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+
+from twilio.base.exceptions import TwilioRestException
+
 from models.challenge_user import ChallengeUserModel
 from twilio.rest import Client
 import os
@@ -21,15 +25,42 @@ if twilio_from_phone is None:
 client = Client(account_sid, auth_token)
 
 
-def send_sms(number):
-    print("sending sms", number)
-    if number is not None:
-        message = client.messages.create(
-            body="Hola mundo",
-            from_=twilio_from_phone,
-            to=number,
-        )
-        print("message sent", message.sid)
+def get_date_from_string(d):
+    return datetime.strptime(d, "%Y-%m-%d %H:%M:%S")
+
+
+def minutes_between(d1, d2):
+    return int(abs((d2 - d1).total_seconds()) / 60)
+
+
+def send_sms(number, challenge_user, is_challenger):
+    user = challenge_user.challenger if is_challenger else challenge_user.challenged
+    user_2 = challenge_user.challenged if is_challenger else challenge_user.challenger
+    now = datetime.utcnow()
+    if number is not None and user is not None and user_2 is not None:
+        challenge = challenge_user.challenge
+        game = challenge.game
+        date_challenge = challenge.date
+        minutes = minutes_between(now, date_challenge)
+        if date_challenge > now:
+            if minutes == 0:
+                portion = "begins now"
+            elif minutes == 1:
+                portion = "begins in a minute"
+            else:
+                portion = f"begins in {minutes} minutes"
+        else:
+            portion = "begins now"
+        message_text = f"Hello, your challenge with the user {user_2.username} in {game.name} {portion}"
+        try:
+            message = client.messages.create(
+                body=message_text,
+                from_=twilio_from_phone,
+                to=number,
+            )
+            print("message sent", message.sid)
+        except TwilioRestException:
+            return False
         return True
     return False
 
@@ -39,14 +70,14 @@ def send_message_challenge_user(challenge_user: ChallengeUserModel):
     if not challenge_user.challenged_sms_sent:
         user = challenge_user.challenged
         if user is not None:
-            sms_sent = send_sms(user.phone)
+            sms_sent = send_sms(user.phone, challenge_user, False)
             if sms_sent:
                 modified = True
                 challenge_user.challenged_sms_sent = True
     if not challenge_user.challenger_sms_sent:
         user = challenge_user.challenger
         if user is not None:
-            sms_sent = send_sms(user.phone)
+            sms_sent = send_sms(user.phone, challenge_user, True)
             if sms_sent:
                 modified = True
                 challenge_user.challenger_sms_sent = True
